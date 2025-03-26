2cec60e40b6e51da46e5a88007c4cbb83d47556c
aghabidareh <aghabidareh@gmail.com>
2025-03-26 00:59:53 -0700

    preprocessing-loading-stopwords-and-initialize-file

--- bidnlp/preprocessing.py
+++ bidnlp/preprocessing.py
@@ -1,9 +1,13 @@
 import re
 import os
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download("stopwords")
 
 class TextPreprocessor:
-    def __init__(self , stopwords_file=None):
-        self.stopwords_file = stopwords_file
+    def __init__(self , stopwords_file=None , language="english"):
+        self.stopwords = set(stopwords.words(language))
         if stopwords_file:
             self.load_stopwords(stopwords_file)
 
