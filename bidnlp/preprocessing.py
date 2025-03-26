c81ceaea9297a20a7a9edc8f02239a390c6cc5d6
aghabidareh <aghabidareh@gmail.com>
2025-03-26 01:24:36 -0700

    preprocessing-lemmatization

--- bidnlp/preprocessing.py
+++ bidnlp/preprocessing.py
@@ -5,6 +5,7 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer, LancasterStemmer , WordNetLemmatizer
 
 nltk.download("stopwords")
+nltk.download("wordnet")
 
 class TextPreprocessor:
     PERSIAN_SUFFIXES = ["ها", "های", "تر", "ترین", "ای", "ام", "ات", "اش", "مان", "تان", "شان"]
