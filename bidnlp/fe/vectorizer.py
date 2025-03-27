2aa14de2fa275a93f20a541f95ee55bd932a259e
aghabidareh <aghabidareh@gmail.com>
2025-03-27 07:46:41 -0700

    feature-extraction-vectorizer-final-commit

--- bidnlp/fe/vectorizer.py
+++ bidnlp/fe/vectorizer.py
@@ -1,12 +1,13 @@
 import os
 import numpy as np
 from collections import Counter
-from sklearn.feature_extraction.text import TfidfVectorizer , CountVectorizer
-from gensim.models import Word2Vec , FastText
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from gensim.models import Word2Vec, FastText
 import joblib
 
+
 class TextVectorizer:
-    def __init__(self , method='tfidf' , max_features=5000 , model_path=None):
+    def __init__(self, method='tfidf', max_features=5000, model_path=None):
         self.method = method.lower()
         self.max_features = max_features
 
@@ -19,13 +20,14 @@ class TextVectorizer:
         else:
             raise ValueError("Invalid vectorization method. Choose 'bow', 'tfidf', 'word2vec', or 'fasttext'.")
 
-    def fit(self , corpus):
-        if self.method in ['bow' , 'tfidf']:
+    def fit(self, corpus):
+        if self.method in ['bow', 'tfidf']:
             self.vectorizer.fit(corpus)
         elif self.method in ['word2vec', 'fasttext']:
             tokenized_sequences = [sentence.split() for sentence in corpus]
             self.model = Word2Vec(sentences=tokenized_sequences, vector_size=100, window=5, min_count=2, workers=4) \
-                if self.method == "word2vec" else FastText(sentences=tokenized_sequences, vector_size=100, window=5, min_count=2, workers=4)
+                if self.method == "word2vec" else FastText(sentences=tokenized_sequences, vector_size=100, window=5,
+                                                           min_count=2, workers=4)
 
     def transform(self, text):
         if self.method in ['bow', 'tfidf']:
