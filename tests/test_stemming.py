392ae415f68fd441802a8bd5faa0b457e4f81748
aghabidareh <aghabidareh@gmail.com>
2025-03-30 10:28:58 -0700

    tests-for-preprocessor-test_stemming_fa

--- tests/test_stemming.py
+++ tests/test_stemming.py
@@ -1,17 +1,21 @@
 import pytest
 from bidnlp.preprocessing import TextPreprocessor
 
+
 @pytest.fixture
 def sample_word_en():
     return "running"
+
 
 @pytest.fixture
 def sample_word_fa():
     return "دویدن"
 
+
 def test_stemming_en(sample_word_en):
     stemmer = TextPreprocessor(language="en")
     assert stemmer.stem(sample_word_en) == "run"
+
 
 def test_stemming_fa(sample_word_fa):
     stemmer = TextPreprocessor(language="fa")
