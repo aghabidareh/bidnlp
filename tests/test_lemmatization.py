c5717376253c69ca86b764986cf3b5f3f44b9860
aghabidareh <aghabidareh@gmail.com>
2025-03-30 10:30:22 -0700

    tests-for-lemmatization

--- tests/test_lemmatization.py
+++ tests/test_lemmatization.py
@@ -12,3 +12,8 @@ def sample_word_fa():
 def test_lemmatization_en(sample_word_en):
     lemmatizer = TextPreprocessor(language="en")
     assert lemmatizer.lemmatize(sample_word_en) == "run"
+
+
+def test_lemmatization_fa(sample_word_fa):
+    lemmatizer = TextPreprocessor(language="fa")
+    assert lemmatizer.lemmatize(sample_word_fa) == "رفتن"
