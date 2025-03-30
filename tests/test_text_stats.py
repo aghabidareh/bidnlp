a3e0c1a596ec358b48a64119d97d7049f70a2116
aghabidareh <aghabidareh@gmail.com>
2025-03-30 10:17:36 -0700

    tests-for-text-stats-test_char_count

--- tests/test_text_stats.py
+++ tests/test_text_stats.py
@@ -4,3 +4,12 @@ from bidnlp.fe.text_stats import TextStats
 @pytest.fixture
 def sample_text_en():
     return "Natural Language Processing is fascinating!"
+
+@pytest.fixture
+def sample_text_fa():
+    return "پردازش زبان طبیعی جذاب است!"
+
+@pytest.mark.parametrize('text' , ['sample_text_en', 'sample_text_fa'])
+def test_char_count(text , request):
+    sample_text = request.getfixturevalue(text)
+    assert sample_text.char_count == len(sample_text)
