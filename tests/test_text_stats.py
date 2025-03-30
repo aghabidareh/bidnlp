0059d32320aea092aefeb1c5fd9b9c04cfb13e0c
aghabidareh <aghabidareh@gmail.com>
2025-03-30 10:20:05 -0700

    tests-for-text-stats-final-commit

--- tests/test_text_stats.py
+++ tests/test_text_stats.py
@@ -1,23 +1,28 @@
 import pytest
 from bidnlp.fe.text_stats import TextStats
 
+
 @pytest.fixture
 def sample_text_en():
     return "Natural Language Processing is fascinating!"
+
 
 @pytest.fixture
 def sample_text_fa():
     return "پردازش زبان طبیعی جذاب است!"
 
-@pytest.mark.parametrize('text' , ['sample_text_en', 'sample_text_fa'])
-def test_char_count(text , request):
+
+@pytest.mark.parametrize('text', ['sample_text_en', 'sample_text_fa'])
+def test_char_count(text, request):
     sample_text = request.getfixturevalue(text)
     assert TextStats.char_count(sample_text) == len(sample_text)
 
-@pytest.mark.parametrize('text, expected' , [('sample_text_en' , 5) , ('sample_text_fa' , 4)])
-def test_word_count(text , expected , request):
+
+@pytest.mark.parametrize('text, expected', [('sample_text_en', 5), ('sample_text_fa', 4)])
+def test_word_count(text, expected, request):
     sample_text = request.getfixturevalue(text)
     assert TextStats.word_count(sample_text) == expected
+
 
 @pytest.mark.parametrize("text", ["sample_text_en", "sample_text_fa"])
 def test_avg_word_length(text, request):
