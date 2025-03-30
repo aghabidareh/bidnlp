abcba51ebf2909c9a8bb5a13edf5c2a3f2bc647a
aghabidareh <aghabidareh@gmail.com>
2025-03-30 10:30:38 -0700

    tests-for-lemmatization-final-commit

--- tests/test_lemmatization.py
+++ tests/test_lemmatization.py
@@ -1,13 +1,16 @@
 import pytest
 from bidnlp.preprocessing import TextPreprocessor
 
+
 @pytest.fixture
 def sample_word_en():
     return "running"
 
+
 @pytest.fixture
 def sample_word_fa():
     return "می‌روم"
+
 
 def test_lemmatization_en(sample_word_en):
     lemmatizer = TextPreprocessor(language="en")
