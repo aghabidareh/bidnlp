7a8f613a3701059f754464779d42a7a9abfb0f33
aghabidareh <aghabidareh@gmail.com>
2025-03-30 10:30:06 -0700

    tests-for-lemmatization

--- tests/test_lemmatization.py
+++ tests/test_lemmatization.py
@@ -8,3 +8,7 @@ def sample_word_en():
 @pytest.fixture
 def sample_word_fa():
     return "می‌روم"
+
+def test_lemmatization_en(sample_word_en):
+    lemmatizer = TextPreprocessor(language="en")
+    assert lemmatizer.lemmatize(sample_word_en) == "run"
