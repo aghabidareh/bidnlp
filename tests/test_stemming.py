da58d127a03ca0bc1c1d443bf71d82d0a84d9e70
aghabidareh <aghabidareh@gmail.com>
2025-03-30 10:28:50 -0700

    tests-for-preprocessor-test_stemming_en

--- tests/test_stemming.py
+++ tests/test_stemming.py
@@ -12,3 +12,7 @@ def sample_word_fa():
 def test_stemming_en(sample_word_en):
     stemmer = TextPreprocessor(language="en")
     assert stemmer.stem(sample_word_en) == "run"
+
+def test_stemming_fa(sample_word_fa):
+    stemmer = TextPreprocessor(language="fa")
+    assert stemmer.stem(sample_word_fa) == "دو"
