116b81dff9641b0f0c015f4868e98dca3728a9fc
aghabidareh <aghabidareh@gmail.com>
2025-03-30 10:26:09 -0700

    tests-for-preprocessor-test_remove_stopwords

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -13,3 +13,8 @@ def test_remove_punctuation(sample_text_en, sample_text_fa):
     preprocessor = TextPreprocessor()
     assert preprocessor.remove_punctuation(sample_text_en) == "This is an example sentence with punctuation"
     assert preprocessor.remove_punctuation(sample_text_fa) == "این یک جمله‌ی نمونه است با علائم نگارشی"
+
+def test_remove_stopwords(sample_text_en, sample_text_fa):
+    preprocessor = TextPreprocessor()
+    assert preprocessor.remove_stopwords(sample_text_en) == "example sentence, punctuation!"
+    assert preprocessor.remove_stopwords(sample_text_fa) == "جمله‌ی نمونه است، علائم نگارشی!"
