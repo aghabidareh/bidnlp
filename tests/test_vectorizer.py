06858a7a5b0d123a6a1f188822db74c77b3de7b7
aghabidareh <aghabidareh@gmail.com>
2025-03-30 10:22:12 -0700

    tests-for-vectorizer-test_tfidf_vectorizer

--- tests/test_vectorizer.py
+++ tests/test_vectorizer.py
@@ -30,7 +30,7 @@ def test_bow_vectorizer(language, texts, request):
 @pytest.mark.parametrize("language, texts", [("en", "sample_texts_en"), ("fa", "sample_texts_fa")])
 def test_tfidf_vectorizer(language, texts, request):
     sample_texts = request.getfixturevalue(texts)
-    vectorizer = TextVectorizer(method="tfidf", max_features=10, lang=language)
+    vectorizer = TextVectorizer(method="tfidf", max_features=10)
     vectorizer.fit(sample_texts)
     transformed = vectorizer.transform(sample_texts[0])
     assert transformed.shape == (1, 10)
