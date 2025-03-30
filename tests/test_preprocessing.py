2e7f5b76fdc353d46d6927e1600105e0175cec57
aghabidareh <aghabidareh@gmail.com>
2025-03-30 10:26:31 -0700

    tests-for-preprocessor-final-commit

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,18 +1,22 @@
 import pytest
 from bidnlp.preprocessing import TextPreprocessor
 
+
 @pytest.fixture
 def sample_text_en():
     return "This is an example sentence, with punctuation!"
+
 
 @pytest.fixture
 def sample_text_fa():
     return "این یک جمله‌ی نمونه است، با علائم نگارشی!"
 
+
 def test_remove_punctuation(sample_text_en, sample_text_fa):
     preprocessor = TextPreprocessor()
     assert preprocessor.remove_punctuation(sample_text_en) == "This is an example sentence with punctuation"
     assert preprocessor.remove_punctuation(sample_text_fa) == "این یک جمله‌ی نمونه است با علائم نگارشی"
+
 
 def test_remove_stopwords(sample_text_en, sample_text_fa):
     preprocessor = TextPreprocessor()
